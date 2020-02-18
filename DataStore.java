1d4a21bb2d89305166a61cb7da7c515fe6f96877
thor <thor@cphbusiness.dk>
2020-02-18 14:35:02 +0100

    uge 8

---
+++ DataStore.java
@@ -0,0 +1,23 @@
+public class DataStore {
+  public static void main(String[] args) {
+
+    // Find the biggest long java provides 
+    // Based on that find a suitable number for storing information
+    // Create the bases (hint: look at https://github.com/cphstud/klassen-uge7/blob/master/jshell-history))
+    // Create a test-case (e.g 1045023072 for storing the two numbers 45 23 and the character 'H')
+    // Make sure you know how to print a character with printf based on the decimal value
+    // (lookup the ASCII-table)
+    // Create the user-dialog for retrieving birthyear, height, weight, initials of name and hometown
+    // Make sure you have space in the big number for each letter 
+    // (create your own location-table, like HER for Herning, CPH for Copenhagen etc)
+    // Tool: When user enters a string you must use String.charAt(<position>) to get to
+    // each character (ThinkJava p. 93). Scanner does not have a nextChar-method.
+    // Organize your code so that you have one method for getting the data from the user and return this as a Long
+    // And another for receiving the Long int and do the decomposition of the different parts. 
+    // By the end this method should display the user-data with a series of printf-statements.
+
+  
+
+  }
+
+}
